f0c2df5a955b301e95e11e234c9c1799a163dbfe
rsheladiya <rajesh.sheladiya@gmail.com>
2017-12-07 16:50:57 -0500

    CheckLink functionality

--- ConsoleApp/LinkChecker.cs
+++ ConsoleApp/LinkChecker.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
@@ -17,5 +19,42 @@ namespace ConsoleApp
                 .Where(l => l.StartsWith("http"));
             return links;
         }
+
+        public static IEnumerable<LinkCheckResult> CheckLinks(IEnumerable<string> links)
+        {
+            var all = Task.WhenAll(links.Select(CheckLink));
+            return all.Result;
+
+        }
+
+        private static async Task<LinkCheckResult> CheckLink(string link)
+        {
+            var result = new LinkCheckResult();
+            result.Link = link;
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Head, link);
+                try
+                {
+                    var response = await client.SendAsync(request);
+                    result.Problem = response.IsSuccessStatusCode ? null : response.StatusCode.ToString();
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    result.Problem = ex.Message;
+                    return result;
+                }
+            }
+        }
     }
+
+    public class LinkCheckResult
+        {
+        public bool Exists => String.IsNullOrWhiteSpace(Problem);
+        public bool Ismissing => !Exists;
+        public string Problem { get; set; }
+        public string Link { get; set; }
+        }
+
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.IO;
 
 namespace ConsoleApp
 {
@@ -9,6 +10,16 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var outputFolder = "Reports";
+            var outputFile = "Report.text";
+            //var outputPath = $"{currentDirectory}/{outputFolder}/{outputFile}";
+            var outputPath = Path.Combine(currentDirectory, outputFolder, outputFile);
+            //var file = Path.GetTempFileName();
+            var directory = Path.GetDirectoryName(outputPath);
+            Console.WriteLine(directory);
+            Directory.CreateDirectory(directory);
+            Console.WriteLine($"Saving report to {outputPath}.");            
             var site = "https://g0t4.github.io/pluralsight-dotnet-core-xplat-apps";
             var clinet = new HttpClient();
             var body = clinet.GetStringAsync(site);
@@ -17,6 +28,18 @@ namespace ConsoleApp
             Console.WriteLine("Links ");
             var links = LinkChecker.GetLinks(body.Result);
             links.ToList().ForEach(Console.WriteLine);
+            //write to file 
+            //File.WriteAllLines(outputPath, links);
+           var checkresult =  LinkChecker.CheckLinks(links);
+            using (var file = File.CreateText(outputPath))
+            {
+                foreach (var item in checkresult.OrderBy(i=>i.Exists))
+                {
+                    var status = item.Ismissing ? "Missing" : "Ok";
+                    file.WriteLine($"{status} - {item.Link}");
+
+                }
+            }
             Console.ReadLine();
 
         }
